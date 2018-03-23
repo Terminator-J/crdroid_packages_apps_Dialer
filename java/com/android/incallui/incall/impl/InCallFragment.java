6d9b04970a2357628aff87ed9edf6d5c3abd534c
Alexey Derendyaev <alexxxdev@yandex.ru>
2018-03-23 10:00:29 +0700

    Dialer: Add ability to set full screen photos for calls
    @neobuddy89: Refactored and cleaned up.
    
    Change-Id: I90ee1c30f7ab248e9fdb8b94d33434c23b327389
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [Updated Dialer merge - Terminator_J]

--- java/com/android/incallui/incall/impl/InCallFragment.java
+++ java/com/android/incallui/incall/impl/InCallFragment.java
@@ -22,6 +22,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.graphics.Insets;
 import android.os.Bundle;
 import android.os.Handler;
@@ -47,6 +49,7 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.preference.PreferenceManager;
 
 import com.android.dialer.R;
 import com.android.dialer.common.Assert;
@@ -62,6 +65,7 @@ import com.android.incallui.incall.impl.ButtonController.CallRecordButtonControl
 import com.android.incallui.incall.impl.ButtonController.SpeakerButtonController;
 import com.android.incallui.incall.impl.ButtonController.UpgradeToRttButtonController;
 import com.android.incallui.incall.impl.InCallButtonGridFragment.OnButtonGridCreatedListener;
+import com.android.incallui.incall.protocol.ContactPhotoType;
 import com.android.incallui.incall.protocol.InCallButtonIds;
 import com.android.incallui.incall.protocol.InCallButtonIdsExtension;
 import com.android.incallui.incall.protocol.InCallButtonUi;
@@ -74,6 +78,7 @@ import com.android.incallui.incall.protocol.PrimaryCallState;
 import com.android.incallui.incall.protocol.PrimaryCallState.ButtonState;
 import com.android.incallui.incall.protocol.PrimaryInfo;
 import com.android.incallui.incall.protocol.SecondaryInfo;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -100,6 +105,7 @@ public class InCallFragment extends Fragment
   private int voiceNetworkType;
   private int phoneType;
   private boolean stateRestored;
+  private View topPhoneContainer;
 
   private final ActivityResultLauncher<String[]> permissionLauncher = registerForActivityResult(
           new ActivityResultContracts.RequestMultiplePermissions(),
@@ -109,6 +115,8 @@ public class InCallFragment extends Fragment
               inCallButtonUiDelegate.callRecordClicked(true);
             }
           });
+
+  private boolean isFullscreenPhoto = false;
 
   // Add animation to educate users. If a call has enriched calling attachments then we'll
   // initially show the attachment page. After a delay seconds we'll animate to the button grid.
@@ -164,8 +172,18 @@ public class InCallFragment extends Fragment
       @Nullable Bundle bundle) {
     LogUtil.i("InCallFragment.onCreateView", null);
     getActivity().setTheme(R.style.Theme_InCallScreen);
-    // Bypass to avoid StrictModeResourceMismatchViolation
-    final View view = layoutInflater.inflate(R.layout.frag_incall_voice, viewGroup, false);
+
+    SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    isFullscreenPhoto = mPrefs.getBoolean("fullscreen_caller_photo", false);
+
+    final int res = isFullscreenPhoto ? R.layout.frag_incall_voice_fullscreen_photo :
+                        R.layout.frag_incall_voice;
+    final View view = layoutInflater.inflate(res, viewGroup, false);
+
+    if (isFullscreenPhoto) {
+      topPhoneContainer = view.findViewById(R.id.incall_contactgrid_container);
+    }
+
     contactGridManager =
         new ContactGridManager(
             view,
@@ -280,6 +298,15 @@ public class InCallFragment extends Fragment
     LogUtil.i("InCallFragment.setPrimary", primaryInfo.toString());
     setAdapterMedia(primaryInfo.multimediaData(), primaryInfo.showInCallButtonGrid());
     contactGridManager.setPrimary(primaryInfo);
+
+    if (topPhoneContainer != null) {
+      boolean hasPhoto = primaryInfo.photo() != null && primaryInfo.photoType() == ContactPhotoType.CONTACT;
+      if (hasPhoto) {
+        topPhoneContainer.setBackgroundColor(0x55000000);
+      } else {
+        topPhoneContainer.setBackgroundColor(Color.TRANSPARENT);
+      }
+    }
   }
 
   private void setAdapterMedia(MultimediaData multimediaData, boolean showInCallButtonGrid) {
