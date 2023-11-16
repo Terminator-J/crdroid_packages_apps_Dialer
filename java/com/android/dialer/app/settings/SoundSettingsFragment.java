crdroidandroid/android_packages_apps_Dialer

Dialer: fixup! 'Dialer: Add autorecord feature'
Change-Id: Id287529822ebc58ee81ffce60b7d2a0e9da939f7

java/com/android/dialer/app/settings/SoundSettingsFragment.java
@@ -21,6 +21,7 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -28,6 +29,7 @@ import android.os.Message;
 import android.os.Vibrator;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
@@ -52,6 +54,11 @@ public class SoundSettingsFragment extends DialerPreferenceFragment
   private static final int MSG_UPDATE_RINGTONE_SUMMARY = 1;
 
   public static final String BUTTON_SMART_MUTE_KEY = "button_smart_mute";
+
+  private static final String AUTO_CALL_RECORDING_KEY = "auto_call_recording";
+
+  private SharedPreferences mPrefs;
+  private SwitchPreference mAutoCallRecording;
 
   private Preference ringtonePreference;
   private final Handler ringtoneLookupComplete =
@@ -92,6 +99,11 @@ public class SoundSettingsFragment extends DialerPreferenceFragment
     addPreferencesFromResource(R.xml.sound_settings);
 
     Context context = getActivity();
+    mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+    mAutoCallRecording = (SwitchPreference) findPreference(AUTO_CALL_RECORDING_KEY);
+    mAutoCallRecording.setChecked(mPrefs.getBoolean(AUTO_CALL_RECORDING_KEY, false));
+    mAutoCallRecording.setOnPreferenceChangeListener(this);
 
     ringtonePreference = findPreference(context.getString(R.string.ringtone_preference_key));
     vibrateWhenRinging =
@@ -219,6 +231,12 @@ public class SoundSettingsFragment extends DialerPreferenceFragment
         // At this time, it is unknown whether the user granted the permission
         return false;
       }
+    } else if (preference == mAutoCallRecording) {
+        boolean value = (Boolean) objValue;
+        mPrefs
+          .edit()
+          .putBoolean(AUTO_CALL_RECORDING_KEY, value)
+          .apply();
     }
     return true;
   }

Terminator-J/crdroid_packages_apps_Dialer

java/com/android/dialer/app/settings/SoundSettingsFragment.java
@@ -24,6 +24,7 @@ import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -42,6 +43,7 @@ import androidx.annotation.Nullable;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -63,6 +65,11 @@ public class SoundSettingsFragment extends PreferenceFragmentCompat
   private static final int MSG_UPDATE_RINGTONE_SUMMARY = 1;
 
   public static final String BUTTON_SMART_MUTE_KEY = "button_smart_mute";
+
+  private static final String AUTO_CALL_RECORDING_KEY = "auto_call_recording";
+
+  private SharedPreferences mPrefs;
+  private SwitchPreferenceCompat mAutoCallRecording;
 
   private DefaultRingtonePreference ringtonePreference;
   private final Handler ringtoneLookupComplete =
@@ -113,6 +120,11 @@ public class SoundSettingsFragment extends PreferenceFragmentCompat
     addPreferencesFromResource(R.xml.sound_settings);
 
     Context context = getActivity();
+    mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+    mAutoCallRecording = (SwitchPreferenceCompat) findPreference(AUTO_CALL_RECORDING_KEY);
+    mAutoCallRecording.setChecked(mPrefs.getBoolean(AUTO_CALL_RECORDING_KEY, false));
+    mAutoCallRecording.setOnPreferenceChangeListener(this);
 
     ringtonePreference = findPreference(context.getString(R.string.ringtone_preference_key));
     vibrateWhenRinging = findPreference(context.getString(R.string.vibrate_on_preference_key));
@@ -228,6 +240,12 @@ public class SoundSettingsFragment extends PreferenceFragmentCompat
         // At this time, it is unknown whether the user granted the permission
         return false;
       }
+    } else if (preference == mAutoCallRecording) {
+        boolean value = (Boolean) objValue;
+        mPrefs
+          .edit()
+          .putBoolean(AUTO_CALL_RECORDING_KEY, value)
+          .apply();
     }
     return true;
   }